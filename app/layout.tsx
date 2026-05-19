de3e8c293a9a9a1b1cafcfeb8b4aa80e8f6677ba
lordson-silver <lordsonochuko5@gmail.com>
2026-05-19 18:30:11 +0100

    feat: integrate Google Tag Manager snippets

--- app/layout.tsx
+++ app/layout.tsx
@@ -123,6 +123,18 @@ export default async function RootLayout({
   return (
     <html lang="en" className={cn("font-sans", geist.variable)}>
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MD7FZ667');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+
         {/* Structured Data – Organization JSON-LD */}
         <script
           type="application/ld+json"
@@ -146,6 +158,16 @@ export default async function RootLayout({
       <body
         className={`${inter.variable} ${hankenGrotesk.variable} antialiased relative`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MD7FZ667"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <BackToTop />
         <WhatsAppButton />
